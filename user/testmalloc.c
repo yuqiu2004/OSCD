97345cf8ea57f87179b15bda8c08f5e45afb0b12
雨不丘 <12752591+kingsyuqiu@user.noreply.gitee.com>
2024-06-01 21:16:40 +0800

    test program for dynamic allocation

--- user/testmalloc.c
+++ user/testmalloc.c
@@ -1,9 +1,10 @@
 #include "user.h"
 
 int main(int argc, char *argv[]){
-    int* a = (int*)malloc(4);
+    int* a = (int*)kalloc(4);
     int b =  16;
     a = &b;
     printf("the addr of a is %d, the value of a is %d\n",a, *a);
+    kfree(a);
     exit(0);
 }
